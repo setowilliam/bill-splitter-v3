cb2b0c076b2b06d2cb8f8db9cba3352151a34f84
William Seto <seto.william@gmail.com>
2022-03-19 20:52:11 -0400

    feat(Button): add `Icon` prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
-import clsx from "../utils/clsx";
+import clsx from "../../utils/clsx";
 import { BUTTON_VARIANT_STYLES, spacing } from "./styles";
 import { ButtonProps } from "./types";
 
 const Button: FC<ButtonProps> = (props) => {
-  const { children, variant = "primary", ...buttonProps } = props;
+  const { children, variant = "primary", Icon, ...buttonProps } = props;
 
   const variantStyle = BUTTON_VARIANT_STYLES[variant];
 
@@ -13,8 +13,9 @@ const Button: FC<ButtonProps> = (props) => {
       {...buttonProps}
       className={`${spacing} ${clsx(
         Object.values(variantStyle)
-      )} w-fit font-medium rounded transition`}
+      )} w-fit font-medium rounded transition flex gap-2 items-center`}
     >
+      {Icon && <Icon />}
       {children}
     </button>
   );
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,7 +1,9 @@
 import { ButtonHTMLAttributes } from "react";
+import { IconType } from "react-icons";
 
 export type ButtonVariant = "primary" | "secondary" | "regular";
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: ButtonVariant;
+  Icon?: IconType;
 };
